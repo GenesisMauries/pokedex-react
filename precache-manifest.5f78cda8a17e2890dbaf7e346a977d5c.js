91314c477e20f505973af15b17bd4a111806ff04
GenesisMauries <exodo_54@hotmail.com>
2019-03-20 11:13:16 -0600

    Updates

---
+++ precache-manifest.5f78cda8a17e2890dbaf7e346a977d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokedex-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "01c750445d0b5a887b35",
+    "url": "/pokedex-react/static/js/runtime~main.ed006230.js"
+  },
+  {
+    "revision": "813822a9ddf552b34ca6",
+    "url": "/pokedex-react/static/js/main.7c384ae5.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/pokedex-react/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "813822a9ddf552b34ca6",
+    "url": "/pokedex-react/static/css/main.74cd1025.chunk.css"
+  },
+  {
+    "revision": "461081edd8c743624ea3b64504c8ab20",
+    "url": "/pokedex-react/index.html"
+  }
+];
